d352428469fbfb3f1c03c494c2f5354851e88651
Giancarlo Prado <giancarlo.prado101@gmail.com>
2026-01-07 17:44:09 +0000

    db and plots

--- IMS/inventory_db.py
+++ IMS/inventory_db.py
@@ -6,10 +6,19 @@ class InventoryDB:
         self.df = pd.DataFrame()
         print("✅ Inventory database initialized.")
 
+    def insert_row(self, row_dict):
+        """
+        Inserta una fila nueva en el inventario.
+        row_dict: diccionario donde las claves son columnas y los valores son los datos.
+        """
+        # Convertir el diccionario a DataFrame de 1 fila
+        row_df = pd.DataFrame([row_dict])
+
+        # Concatenar con el DataFrame interno, agregando columnas dinámicamente
+        self.df = pd.concat([self.df, row_df], ignore_index=True)
+
     def show(self):
         """
         Muestra el DataFrame de inventario actual.
         """
-        print("Current Inventory Database:"
-              f"\n{self.df}")
         return self.df
